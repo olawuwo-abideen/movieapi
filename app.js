85447c0ddca3e58cc888dc5fd2eac5641576f113
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-17 15:43:54 +0100

    get

--- app.js
+++ app.js
@@ -18,28 +18,14 @@ app.get("/", (req, res) => {
 
 // get endpoint
 
-app.get("/api/action", (req, res) => {
-  res.send(action);
-});
-
-app.get("/api/comedy", (req, res) => {
+app.get("/api/genre/comedy", (req, res) => {
   res.send(comedy);
 });
 
-app.get("/api/disaster", (req, res) => {
-  res.send(disaster);
-});
-
-app.get("/api/history", (req, res) => {
-  res.send(history);
-});
-
-app.get("/api/horror", (req, res) => {
-  res.send(horror);
-});
-
-app.get("/api/scifi", (req, res) => {
-  res.send(sci_fi);
+app.get('/api/genre/action/:id', (req, res) => {
+  const findAction = action.find(c => c.id === parseInt(req.params.id));
+  if (!findAction) return res.status(404).send('The movie with the given ID was not found')
+  res.send(findAction);
 });
 
 app.get("/api/developer", (req, res) => {
@@ -47,9 +33,119 @@ app.get("/api/developer", (req, res) => {
 });
 
 
-app.get("/api/developer/:name", (req, res) => {
-  res.send(developer);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
